9d6c0d2503141e89cceb473c8a19372026bd0d76
Hemant D <hemant132us@gmail.com>
2022-12-06 02:56:43 +0530

    test3

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,13 @@ export default function Home({data}) {
         <title class="tite">This is a test m</title>
         <meta name="description" content=" " />
         <meta property="og:title"  content="" />
+        <meta property="og:image" content="https://example.com/ogp.jpg" />
+        <meta property="og:image" content="https://example.com/ogp.jpg" />
+        <meta property="og:image:secure_url" content="https://secure.example.com/ogp.jpg" />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:width" content="400" />
+        <meta property="og:image:height" content="300" />
+        <meta property="og:image:alt" content="A shiny red apple with a bite taken out" />
 
         <meta
             property="og:image"
