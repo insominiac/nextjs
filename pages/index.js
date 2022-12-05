fe6dc751ac69ded53dcef45fb9ebdcfebcf2efa3
Hemant D <hemant132us@gmail.com>
2022-12-05 22:03:22 +0530

    json

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import Image from 'next/image'
 //import pic from 'https://www.shtypketu.com/wp-content/uploads/2022/12/test1.jpg'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({data}) {
+  console.log(data)
   return (
     <div className={styles.container}>
       <Head>
@@ -14,77 +15,24 @@ export default function Home() {
         <meta
             property="og:image"
             content="/test2.jpeg"
-            
            />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://insominiac.vercel.app/?fbclid=IwAR0bm7noVreRtm5KfAEz72iLeJkkXbpbinjua4WeZAoNgbk76e_B_TzRUpY"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <Image src="/vercel.svg" alt="Vercel Logo" />
-
-
-          </a>
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <Image src="/test3.jpeg" alt="Vercel Logo" />
-          
-
-
-          </a>
-
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
+}
+
+export async function getServerSideProps(context){
+  const res = await fetch(`https://www.shtypketu.com/wp-json/wp/v2/posts/`)
+  const data = await res.json()
+  if(!data) {
+    return{
+      notFound: true,
+    }
+  }
+  return{
+    props:{
+      data
+    },
+  }
 }
