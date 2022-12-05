0af9fe3c59f277817814be2021acc477850e8907
Hemant D <hemant132us@gmail.com>
2022-12-05 15:01:00 +0530

    Blank Title

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       <Head>
         <title>This is a test</title>
         <meta name="description" content=" " />
-        <meta property="og:title"  content=" " />
+        <meta property="og:title"  content="&nbsp;" />
 
         <meta
             property="og:image"
