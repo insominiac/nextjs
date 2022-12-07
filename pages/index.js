aeb89f13d4cd32257261958e32de1e868cace103
Hemant D <hemant132us@gmail.com>
2022-12-08 00:35:25 +0530

    og

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({data}) {
         <meta property="og:title"  content="&nbsp;" />
         <meta property="og:url"  content="/" />
 
-        <meta property="og:image" content="/test2.jpeg" />
+        <meta property="og:image" content="https://www.shtypketu.com/wp-content/uploads/2022/12/test1.jpg" />
         <meta property="og:image:type" content="image/jpeg" />
         <meta property="og:image:width" content="800" />
         <meta property="og:image:height" content="800" />
