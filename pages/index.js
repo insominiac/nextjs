0d8bac29e8ae9ad6694826484646750a46a50cad
Hemant D <hemant132us@gmail.com>
2022-12-11 23:48:06 +0530

    yrt

--- pages/index.js
+++ pages/index.js
@@ -38,9 +38,8 @@ export default function Home({data}) {
       {postHtml}
        <span> This is a text</span>
      
-      <a href="https://example.com">
+      <link href="https://www.shtypketu.com/wp-content/uploads/2022/12/test1.jpg">test</link>
        
-      </a>
     </div>
   )
 }
