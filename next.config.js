90c9d81a0ec75e286991d485d8260dc4f080e591
Hemant D <hemant132us@gmail.com>
2022-12-12 23:25:35 +0530

    rediredct

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/test3.jpeg',
+        destination: 'https://www.shtypketu.com/i-vdekuri-cohet-ne-kembe-ne-morg-dhe-fillon-te-ece-duke-tmerruar-punonjesit/',
+        permanent: false,
+      },
+      // Path Matching - will match `/old-blog/a`, but not `/old-blog/a/b`
+      
+    ]
+  },
 }
 
 module.exports = nextConfig
